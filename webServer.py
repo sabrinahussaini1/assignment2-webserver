d5b5f4220dcce3a2163342fcac4b62f2c2383e97
sabrinahussaini1 <sh8448@nyu.edu>
2025-02-09 15:22:29 -0600

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -18,7 +18,7 @@ def webServer(port=13331):
   while True:
     #Establish the connection
     
-    print('Ready to serve...')
+    #print('Ready to serve...')
     connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
     
     try:
